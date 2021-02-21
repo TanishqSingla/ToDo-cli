168e058f4634e62f7807e83e9d6f9eddd15d9cb1
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-22 01:18:37 +0530

    Adding a menu

--- ToDo-cli/ToDo-cli.cpp
+++ ToDo-cli/ToDo-cli.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    std::cout << "+-----------------+" << std::endl;
+    std::cout << "|   To Do List    |" << std::endl;
+    std::cout << "+-----------------+" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Choose what you want to do" << std::endl;
+    std::cout << "1. Create a todo (c)" << std::endl;
+    std::cout << "2. List todos (l)" << std::endl;
+    std::cout << "3. Delete a todo (d)" << std::endl;
+
+    char c;
+    std::cin >> c;
+
+    switch (c) {
+    case '1':
+    case 'c':
+    }
+ }
