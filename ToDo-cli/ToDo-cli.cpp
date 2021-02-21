af55940f8355e0e7d750ff75374f95ac52d4df7e
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-22 01:51:06 +0530

    Adding exit option in menu Adding Todo linked list and tested list function

--- ToDo-cli/ToDo-cli.cpp
+++ ToDo-cli/ToDo-cli.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <string>
+#define MAXCHARLEN 256
+
+struct Todo {
+    char todo[MAXCHARLEN];
+    Todo* next;
+};
+
 
 int main()
 {
+    Todo* head = NULL;
     std::cout << "+-----------------+" << std::endl;
     std::cout << "|   To Do List    |" << std::endl;
     std::cout << "+-----------------+" << std::endl;
     std::cout << std::endl;
     std::cout << "Choose what you want to do" << std::endl;
-    std::cout << "1. Create a todo (c)" << std::endl;
+    std::cout << "1. Add a todo (a)" << std::endl;
     std::cout << "2. List todos (l)" << std::endl;
     std::cout << "3. Delete a todo (d)" << std::endl;
+    std::cout << "4. Exit application (e)" << std::endl;
 
     char c;
     std::cin >> c;
 
-    switch (c) {
-    case '1':
-    case 'c':
+    if (c == 'a') {
+        if (head == NULL) {
+            head = new Todo();
+            std::cin.getline(head->todo, sizeof(head->todo));
+        }
+        else if (head->next == NULL) {
+            Todo* next = new Todo();
+            head->next = next;
+        }
+    }
+
+    if (c == 'l') {
+        if (head == NULL) {
+            std::cout << "There are no todos";
+        }
     }
  }
